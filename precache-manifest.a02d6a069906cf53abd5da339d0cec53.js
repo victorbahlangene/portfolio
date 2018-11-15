e0280655bfe9ba643b1a3f3fd4dbb9b9add87b4c
Victor Bahlangene <victorBahlangene96@gmail.com>
2018-11-15 22:03:43 +0300

    Updates

---
+++ precache-manifest.a02d6a069906cf53abd5da339d0cec53.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "13d118c708a518c3d8b6",
+    "url": "/portfolio/static/css/main.3203d120.chunk.css"
+  },
+  {
+    "revision": "13d118c708a518c3d8b6",
+    "url": "/portfolio/static/js/main.13d118c7.chunk.js"
+  },
+  {
+    "revision": "6b1ab41cd37992a14a9a",
+    "url": "/portfolio/static/css/1.2018b161.chunk.css"
+  },
+  {
+    "revision": "6b1ab41cd37992a14a9a",
+    "url": "/portfolio/static/js/1.6b1ab41c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "af5ec62b6042cf2507b5cf1b21359a5c",
+    "url": "/portfolio/static/media/engineering.af5ec62b.png"
+  },
+  {
+    "revision": "dd8f7dd0cceec6778a04eede4bd981ab",
+    "url": "/portfolio/static/media/weatherApp.dd8f7dd0.png"
+  },
+  {
+    "revision": "a4e60d614be67a004a6d8ffe5b81ea26",
+    "url": "/portfolio/static/media/twitchApp.a4e60d61.png"
+  },
+  {
+    "revision": "bbb99e7cb9d0e07cfeefd8b54e5d1412",
+    "url": "/portfolio/static/media/wikipediaApp.bbb99e7c.png"
+  },
+  {
+    "revision": "97626e4640c68f4bdcb05093f02e3f87",
+    "url": "/portfolio/static/media/location1.97626e46.png"
+  },
+  {
+    "revision": "3e1919448ba5fdc49e35ecbff4b4b095",
+    "url": "/portfolio/static/media/showcase.3e191944.jpg"
+  },
+  {
+    "revision": "37e9992ba36aaff577b3f4abbc90bb45",
+    "url": "/portfolio/index.html"
+  }
+];
